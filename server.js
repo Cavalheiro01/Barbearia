9fe7ff8d9978a8e4cd32c190cf70ac758671cb10
RODRIGO CAVALHEIRO CORREA FLORIANO MARQUES <rodrigo.ccfmarques@senacsp.edu.br>
2026-06-15 16:37:21 -0300

    Corrige rota frontend no Render

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
  
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use("/frontend", express.static(path.join(__dirname, "frontend")));
  
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "paginaInicial.html"));
