1683a6a83a73a1a7901ba2b5b81b4d1d3db43547
RODRIGO CAVALHEIRO CORREA FLORIANO MARQUES <rodrigo.ccfmarques@senacsp.edu.br>
2026-06-16 16:54:59 -0300

    Alteração do server.js

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
  
-app.use("/frontend", express.static(path.join(__dirname, "frontend")));
+// app.use("/frontend", express.static(path.join(__dirname, "frontend")));
  
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "paginaInicial.html"));
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "paginaInicial.html"));
+// });
+ 
+ 
  
 const clientesRoutes = require("./backend/routes/cliente");
 const adminRoutes = require("./backend/routes/admin");
